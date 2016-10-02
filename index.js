93ee8a1d8780b6a679b10bf76da3eb97ea838c7e
Tarun Jangra <tarun@izap.in>
2016-10-02 14:48:34 +0530

    youtube api to search in all videos. you can pass how many records you are needed

---
+++ index.js
@@ -0,0 +1,25 @@
+var axios = require('axios');
+
+var ROOT_URL = 'https://www.googleapis.com/youtube/v3/search';
+
+module.exports = function (options, callback) {
+  if (!options.key) {
+    throw new Error('Provide valid youtube developer key to trigger search.');
+  }
+
+  var params = {
+    part: 'snippet',
+    key: options.key,
+    q: options.term,
+    type: 'video',
+    maxResults: options.maxResults?options.maxResults:10
+  };
+
+  axios.get(ROOT_URL, { params: params })
+    .then(function(response) {
+      if (callback) { callback(response.data.items); }
+    })
+    .catch(function(error) {
+      console.error(error);
+    });
+};
